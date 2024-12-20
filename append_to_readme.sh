22fb1bcff82ce12c8d67097425e93bac408a31b4
Paul Gasbarra <paulgasbarra@admins-air.lan>
2024-12-20 11:41:19 -0500

    remove shell scripts

--- append_to_readme.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Find all readme.md files in the subdirectories of the current folder
-find . -type f -iname 'readme.md' | while read -r file; do
-    # Append the text to each file
-    echo -e "\n## Additional Resources" >> "$file"
-done
