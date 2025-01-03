06b4e0cb97a6b027e95c1e227a2808f7758ddc1a
Paul Gasbarra <paulgasbarra@admins-air.lan>
2025-01-03 15:14:59 -0500

    add in lesson 10

---
+++ lesson10-big-o-notation/problem.js
@@ -0,0 +1,10 @@
+/**
+ * Problem: Implement a function that determines if a given year is a leap year. A leap year is a year that is divisible by 4, but not divisible by 100, unless it is divisible by 400. For example, 2000 is a leap year, but 1900 is not.
+ * This should ideally run in O(1) time.
+ */
+
+function isLeapYear(year) {
+  // TODO: Implement the function
+}
+
+module.exports = isLeapYear;
---
+++ lesson10-big-o-notation/problem.test.js
@@ -0,0 +1,11 @@
+const isLeapYear = require("/Users/paulgasbarra/Code/pursuit/dsa-coursework-l3-2025/lesson10-big-o-notation/problem");
+
+// Test cases
+console.log(isLeapYear(2000)); // Expected output: true
+console.log(isLeapYear(1900)); // Expected output: false
+console.log(isLeapYear(2024)); // Expected output: true
+console.log(isLeapYear(2021)); // Expected output: false
+console.log(isLeapYear(1600)); // Expected output: true
+console.log(isLeapYear(1700)); // Expected output: false
+console.log(isLeapYear(1800)); // Expected output: false
+console.log(isLeapYear(2100)); // Expected output: false
